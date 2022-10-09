5919bda09811112530a82b1b44cf574e77739a32
systemshock89 <system.shock89@gmail.com>
2022-10-09 21:46:04 +0300

    timer

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    // Tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -41,5 +42,70 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
     });
+    // /Tabs
+
+    // Timer
+    const deadline = '2023-05-11';
+
+    function getTimeRemaining(endtime){
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+
+            // Math.floor округление до ближайшего целого
+            // (1000 * 60 * 60 * 24) сколько в сутках миллисекунд
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+
+            // 1000 * 60 * 60 делим на кол-во миллисекунд в одном часе
+            // чтобы не получить 100 часов, нужно исп-ть %
+            // % делит на 24 и возвращает остаток от деления
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minites = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000 ) % 60);
+
+            return {
+                'total': t,
+                'days': days,
+                'hours': hours,
+                'minutes': minites,
+                'seconds': seconds
+
+            };
+    }
+
+    // подсталяем 0 к цифрам, если они < 10, чтобы было напр 01
+    function getZero(num){
+        if(num >=0 && num < 10){
+            return `0${num}`;
+        } else{
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime){
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        // чтобы не было мигания верстки с цифрами из верстки (тк по интервалу эта ф-я запустится только через 1 сек)
+        updateClock();
+        
+        function updateClock(){
+            const t = getTimeRemaining(endtime); // разница между планируемым временем и текущим
+            
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if(t.total <= 0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+    // /Timer
 
 });
