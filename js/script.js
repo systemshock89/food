6d4700e3f472cb14071f70f43548b5dc014ccb77
systemshock89 <system.shock89@gmail.com>
2022-10-10 19:10:25 +0300

    modal fix

--- js/script.js
+++ js/script.js
@@ -203,13 +203,18 @@ window.addEventListener('DOMContentLoaded', () => {
   const modalTrigger = document.querySelectorAll('[data-modal]'),
         modal = document.querySelector('.modal'),
         modalCloseBtn = document.querySelector('[data-close]');
-  modalTrigger.forEach(btn => {
-    btn.addEventListener('click', () => {
-      modal.classList.add('show');
-      modal.classList.remove('hide'); // modal.classList.toggle('show');
 
-      document.body.style.overflow = 'hidden'; // убираем прокрутку страницы про открытом попапе
-    });
+  function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide'); // modal.classList.toggle('show');
+
+    document.body.style.overflow = 'hidden'; // убираем прокрутку страницы про открытом попапе
+
+    clearInterval(modalTimerId); // если пользователь сам открыл попап, то уже не показываем ему попап снова через 5 сек
+  }
+
+  modalTrigger.forEach(btn => {
+    btn.addEventListener('click', openModal);
   });
 
   function closeModal() {
@@ -232,7 +237,24 @@ window.addEventListener('DOMContentLoaded', () => {
     if (e.code === 'Escape' && modal.classList.contains('show')) {
       closeModal();
     }
-  }); // /Modal
+  }); // показывать попап через 5 сек после открытия страницы
+
+  const modalTimerId = setTimeout(openModal, 5000);
+
+  function showModalByScroll() {
+    // pageYOffset - сколько px отлистал пользователь по оси Y
+    // document.documentElement.clientHeight - видимая часть, кот-ю видим на данный момент на сайте без прокрутки
+    // document.documentElement.scrollHeight - высота с учетом прокрутки
+    // в конце добавим -1px, тк на некоторых мониторах и браузерах без этого не срабатывает
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+      openModal(); // как только один раз пользователь долистал до конца и ему показался попап, обработчик удаляется, чтоб больше попап не показывался
+
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  } // когда пользователь долистает до самого конца страницы - показать попап
+
+
+  window.addEventListener('scroll', showModalByScroll); // /Modal
 });
 
 /***/ })
