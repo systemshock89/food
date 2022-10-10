a368e85dabcd5b9e3a8a3490fe368ea8be0541a6
systemshock89 <system.shock89@gmail.com>
2022-10-10 18:47:49 +0300

    modal

--- src/js/main.js
+++ src/js/main.js
@@ -117,4 +117,42 @@ window.addEventListener('DOMContentLoaded', () => {
     setClock('.timer', deadline);
     // /Timer
 
+    // Modal
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            // modal.classList.toggle('show');
+            document.body.style.overflow = 'hidden'; // убираем прокрутку страницы про открытом попапе
+        });
+    });
+   
+    function closeModal(){
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+         // modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    // при клике на подложку закрываем окно
+    modal.addEventListener('click', (e) => {
+        if(e.target === modal){
+            closeModal();
+        }
+    });
+
+    // закрываем окно при нажатии на клавишу Escape
+    document.addEventListener('keydown', (e) => { // keyDown - срабатывает когда нажимается кнопка
+        if(e.code === 'Escape' && modal.classList.contains('show') ){
+            closeModal();
+        }
+    });
+    // /Modal
+
 });
