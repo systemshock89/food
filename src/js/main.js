c79e94e82e95f1a7c845331f2b8720c41ec7b4a8
systemshock89 <system.shock89@gmail.com>
2022-10-16 16:13:02 +0300

    showThanksModal

--- src/js/main.js
+++ src/js/main.js
@@ -121,8 +121,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Modal
     const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector('[data-close]');
+        modal = document.querySelector('.modal');
 
     function openModal(){
         modal.classList.add('show');
@@ -143,11 +142,9 @@ window.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = '';
     }
 
-    modalCloseBtn.addEventListener('click', closeModal);
-
-    // при клике на подложку закрываем окно
+    // при клике на подложку, либо при клике на крестик закрываем окно
     modal.addEventListener('click', (e) => {
-        if(e.target === modal){
+        if(e.target === modal || e.target.getAttribute('data-close') == ''){
             closeModal();
         }
     });
@@ -159,8 +156,8 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // показывать попап через 5 сек после открытия страницы
-    const modalTimerId = setTimeout(openModal, 5000);
+    // показывать попап через 50 сек после открытия страницы
+    const modalTimerId = setTimeout(openModal, 50000);
 
     function showModalByScroll(){
         // pageYOffset - сколько px отлистал пользователь по оси Y
@@ -262,7 +259,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const forms = document.querySelectorAll('form');
 
     const message = {
-        loading: 'Загрузка',
+        loading: 'img/form/spinner.svg',
         success: 'Спасибо! Скоро мы с Вами свяжемся',
         failture: 'Что-то пошло не так...'
     };
@@ -320,10 +317,13 @@ window.addEventListener('DOMContentLoaded', () => {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
-            const statusMessage = document.createElement('div');
-            statusMessage.classList.add('status');
-            statusMessage.textContent = message.loading;
-            form.append(statusMessage);
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
+            form.insertAdjacentElement('afterend', statusMessage);
 
             const request = new XMLHttpRequest();
             request.open('POST', 'server.php');
@@ -348,17 +348,42 @@ window.addEventListener('DOMContentLoaded', () => {
             request.addEventListener('load', () => {
                 if (request.status === 200){
                     console.log(request.response);
-                    statusMessage.textContent = message.success;
+                    showThanksModal(message.success);
                     form.reset();
-                    setTimeout(() => {
-                        statusMessage.remove();
-                    }, 2000);
+                    statusMessage.remove();
                 } else {
-                    statusMessage.textContent = message.failture;
+                    showThanksModal(message.failture);
                 }
             });
 
         });
+    }
+
+    // ф-я показывающая сообщения после отправки формы
+    function showThanksModal(message){
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+        openModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div data-close class="modal__close">&times;</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+        
+        document.querySelector('.modal').append(thanksModal);
+
+        // спустя 4 сек убирать сообщение и возвращать форму к изначальному состоянию
+        setTimeout(() =>{
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+        }, 4000);
     }
     // /Forms
 
