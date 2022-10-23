2593ca037debabdc9f9c68d51416f099c681e171
systemshock89 <system.shock89@gmail.com>
2022-10-24 00:28:24 +0300

    lesson 5.14 slider dots

--- js/script.js
+++ js/script.js
@@ -3317,6 +3317,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
   const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
@@ -3342,7 +3343,28 @@ window.addEventListener('DOMContentLoaded', () => {
   slidesWrapper.style.overflow = 'hidden';
   slides.forEach(slide => {
     slide.style.width = width; // чтобы все слайды были одинаковой ширины
-  });
+  }); // dots
+
+  slider.style.position = 'relative';
+  const indicators = document.createElement('ol'),
+        dots = [];
+  indicators.classList.add('carousel-indicators');
+  slider.append(indicators);
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.classList.add('dot');
+    indicators.append(dot); // активный эл-т
+
+    if (i == 0) {
+      dot.style.opacity = 1;
+    }
+
+    dots.push(dot); // получим массив 
+  } // /dots
+
+
   next.addEventListener('click', () => {
     // если долистали до самого конца, то возвратимся в начало
     if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
@@ -3366,6 +3388,9 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       current.textContent = `slideIndex`;
     }
+
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[slideIndex - 1].style.opacity = '1';
   });
   prev.addEventListener('click', () => {
     // если долистали до самого начала, то возвратимся в конец слайдера
@@ -3389,6 +3414,26 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       current.textContent = `slideIndex`;
     }
+
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[slideIndex - 1].style.opacity = '1';
+  });
+  dots.forEach(dot => {
+    dot.addEventListener('click', e => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+      slideIndex = slideTo;
+      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      if (slides.length < 10) {
+        current.textContent = `0${slideIndex}`;
+      } else {
+        current.textContent = `slideIndex`;
+      }
+
+      dots.forEach(dot => dot.style.opacity = '.5');
+      dots[slideIndex - 1].style.opacity = '1';
+    });
   }); // Slider variant 2
   // Slider variant 1
   // const slides = document.querySelectorAll('.offer__slide'),
