cd9e7097c5ef5cc3c545aaa66d70224dd67ec886
systemshock89 <system.shock89@gmail.com>
2022-10-27 00:16:46 +0300

    lesson 5.17 calc part 1

--- src/js/main.js
+++ src/js/main.js
@@ -707,4 +707,79 @@ window.addEventListener('DOMContentLoaded', () => {
     // });
     // /Slider variant 1
 
+
+    // Calc
+    const result = document.querySelector('.calculating__result span');
+    let sex = 'female', 
+        height, weight, age, 
+        ratio = 1.375;
+        // установим дефолтные значения для sex и ratio, т.к. в макете они активны
+
+    // пересчитывает рез-т
+    function calcTotal(){
+        // если каких-то данных недостает
+        if(!sex || !height || !weight || !age || !ratio){
+            result.textContent = '____'; // то выводим пустую строку
+            return; // и преращаем работу ф-и
+        }
+
+        if(sex === 'female'){
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+
+    calcTotal();
+
+    // получение инф-ции со статических блоков
+    function getStaticInformation(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+
+                if(e.target.getAttribute('data-ratio')){
+                    ratio = +e.target.getAttribute('data-ratio');
+                } else {
+                    sex = e.target.getAttribute('id');
+                }
+    
+                elements.forEach(elem => elem.classList.remove(activeClass));
+                e.target.classList.add(activeClass);
+    
+                calcTotal(); // пересчитаем рез-т
+            });
+        });
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active'); // для пола
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active'); // для физ. активности
+
+    // получение динам-й инф-ции с инпутов
+    function getDynamicInformation(selector){
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            switch(input.getAttribute('id')){
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;   
+                case 'age':
+                    age = +input.value;
+                    break;                                      
+            }
+
+            calcTotal(); // пересчитаем рез-т
+        });
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
+    // /Calc
+
 });
