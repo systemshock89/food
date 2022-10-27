f0195dbb72ef0e413f4d2090411036860ed1b87b
systemshock89 <system.shock89@gmail.com>
2022-10-28 01:04:31 +0300

    lesson 5.18. calc localstorage

--- js/script.js
+++ js/script.js
@@ -4173,12 +4173,42 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
   const result = document.querySelector('.calculating__result span');
-  let sex = 'female',
-      height,
-      weight,
-      age,
-      ratio = 1.375; // установим дефолтные значения для sex и ratio, т.к. в макете они активны
-  // пересчитывает рез-т
+  let sex, height, weight, age, ratio;
+
+  if (localStorage.getItem('sex')) {
+    sex = localStorage.getItem('sex');
+  } else {
+    // установим дефолтные значения для sex и ratio, т.к. в макете они активны
+    // и запишем их в localStorage
+    sex = 'female';
+    localStorage.setItem('sex', 'female');
+  }
+
+  if (localStorage.getItem('ratio')) {
+    ratio = localStorage.getItem('ratio');
+  } else {
+    ratio = 1.375;
+    localStorage.setItem('ratio', 1.375);
+  } // ф-я инициализирует активные элементы в соответствии с данными из localStorage
+
+
+  function initLocalSettings(selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(elem => {
+      elem.classList.remove(activeClass);
+
+      if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+        elem.classList.add(activeClass);
+      }
+
+      if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+        elem.classList.add(activeClass);
+      }
+    });
+  }
+
+  initLocalSettings('#gender div', 'calculating__choose-item_active');
+  initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active'); // пересчитывает рез-т
 
   function calcTotal() {
     // если каких-то данных недостает
@@ -4197,14 +4227,16 @@ window.addEventListener('DOMContentLoaded', () => {
 
   calcTotal(); // получение инф-ции со статических блоков
 
-  function getStaticInformation(parentSelector, activeClass) {
-    const elements = document.querySelectorAll(`${parentSelector} div`);
+  function getStaticInformation(selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
     elements.forEach(elem => {
       elem.addEventListener('click', e => {
         if (e.target.getAttribute('data-ratio')) {
           ratio = +e.target.getAttribute('data-ratio');
+          localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
         } else {
           sex = e.target.getAttribute('id');
+          localStorage.setItem('sex', e.target.getAttribute('id'));
         }
 
         elements.forEach(elem => elem.classList.remove(activeClass));
@@ -4214,14 +4246,21 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  getStaticInformation('#gender', 'calculating__choose-item_active'); // для пола
+  getStaticInformation('#gender div', 'calculating__choose-item_active'); // для пола
 
-  getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active'); // для физ. активности
+  getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active'); // для физ. активности
   // получение динам-й инф-ции с инпутов
 
   function getDynamicInformation(selector) {
     const input = document.querySelector(selector);
     input.addEventListener('input', () => {
+      // Валидация. если введены не числа
+      if (input.value.match(/\D/g)) {
+        input.style.border = '1px solid red';
+      } else {
+        input.style.border = 'none';
+      }
+
       switch (input.getAttribute('id')) {
         case 'height':
           height = +input.value;
